ec4fbd5bdc7f23175646860909ae51d35a56afce
Halil Yıldırım <71269065+ydmhalil@users.noreply.github.com>
2023-11-22 19:45:10 +0300

    superset.py

---
+++ superset.py
@@ -0,0 +1,10 @@
+kume1 = set(["data", "python"])
+kume2 = set(["data", "function", "qcut", "lambda", "python", "miuul"])
+
+def kume(set1,set2):
+    if set1.issuperset(set2):
+        print(set1.intersection(set2))
+    else:
+        print(set2.difference(set1))
+
+kume(kume1,kume2)
